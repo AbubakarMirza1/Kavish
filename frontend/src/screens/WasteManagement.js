309d897edaad541f5bd4e573b132701d5de2ac50
musaibuf <97942670+musaibuf@users.noreply.github.com>
2024-12-14 01:22:17 +0500

    Update WasteManagement.js

--- frontend/src/screens/WasteManagement.js
+++ frontend/src/screens/WasteManagement.js
@@ -103,8 +103,12 @@ const WasteManagement = () => {
 
   const handleSidebarClick = (section) => {
     setSelectedSection(section);
-    if (section === 'data-entry') navigate('/Scope1SC');
-    if (section === 'dashboard') navigate('/dashboard');
+      if (section === 'dashboard') navigate('/dashboard');
+      if (section === 'emissions') navigate('/GHGEmissions');
+      if (section === 'waste') navigate('/WasteManagement');
+      if (section === 'data-entry') navigate('/Scope1SC');
+      if (section === 'reports') navigate('/Reports');
+      if (section === 'analytics') navigate('/analytics');
   };
 
   return (
